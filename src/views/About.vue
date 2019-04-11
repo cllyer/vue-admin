deb95e0256b0f289dd62e530e0d04f4c55070f5b
chenlong <72051211@vivo.xyz>
2019-04-11 16:47:45 +0800

    svgIcon

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <icon-svg icon-class="user"></icon-svg>
   </div>
 </template>
---
+++ vue.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+// const ip = require('ip')
+
+// const isProd = process.env.NODE_ENV === 'production'
+// const isTest = process.env.ENV === 'testServer'
+
+function resolve (dir) {
+  return path.join(__dirname, '.', dir)
+}
+
+module.exports = {
+  // publicPath: '/',
+  configureWebpack: {
+    externals: {
+      'BMap': 'BMap'
+    },
+    plugins: []
+  },
+  chainWebpack: config => {
+    // config.resolve.alias.set('@', resolve('src'))
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons/svg'))
+      .end()
+
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons/svg'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({ symbolId: 'icon-[name]' })
+  },
+  devServer: {
+    // host: ip.address(),
+    open: true, // 自动打开浏览器
+    port: 8888
+    // proxy: {
+    //   '/api': {
+    //     target: '<url>',
+    //     ws: true,
+    //     changeOrigin: true
+    //   }
+    // },
+  }
+}
